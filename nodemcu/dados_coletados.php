2e689f531fb535a35d741974bc1b86550b1ec3eb
Pedro Gustavo <pedrogustavosantosdelima@gmail.com>
2021-03-07 19:52:03 -0300

    pagina 'dados recebidos' concluida

--- nodemcu/dados_coletados.php
+++ nodemcu/dados_coletados.php
@@ -58,8 +58,8 @@
                 <div class="monthYear">
                     <button onClick="VoltarMes()">&#60;</button>
                     <div>
-                        <p id= "month" class = "month">Março</p>
-                        <p id= "year" class = "year" >2021</p>
+                        <p id= "Month" class = "month">Março</p>
+                        <p id= "Year" class = "year" >2021</p>
                     </div>
                     <button onClick="AvancarMes()">&#62;</button>
                 </div>
@@ -74,60 +74,67 @@
                 </div>
 
                 <div class="days">
-                    <button id='1' class = "notActive"></button>
-                    <button id='2' class = "notActive"></button>
-                    <button id='3' class = "notActive"></button>
-                    <button id='4' class = "notActive"></button>
-                    <button id='5' class = "notActive"></button>
-                    <button id='6' class = "notActive"></button>
-                    <button id='7' class = "notActive"></button>
-                    <button id='8' class = "notActive"></button>
-                    <button id='9' class = "notActive"></button>
-                    <button id='10' class = "notActive"></button>
-                    <button id='11' class = "notActive"></button>
-                    <button id='12' class = "notActive"></button>
-                    <button id='13' class = "notActive"></button>
-                    <button id='14' class = "notActive"></button>
-                    <button id='15' class = "notActive"></button>
-                    <button id='16' class = "notActive"></button>
-                    <button id='17' class = "notActive"></button>
-                    <button id='18' class = "notActive"></button>
-                    <button id='19' class = "notActive"></button>
-                    <button id='20' class = "notActive"></button>
-                    <button id='21' class = "notActive"></button>
-                    <button id='22' class = "notActive"></button>
-                    <button id='23' class = "notActive"></button>
-                    <button id='24' class = "notActive"></button>
-                    <button id='25' class = "notActive"></button>
-                    <button id='26' class = "notActive"></button>
-                    <button id='27' class = "notActive"></button>
-                    <button id='28' class = "notActive"></button>
-                    <button id='29' class = "notActive"></button>
-                    <button id='30' class = "notActive"></button>
-                    <button id='31' class = "notActive"></button>
-                    <button id='32' class = "notActive"></button>
-                    <button id='33' class = "notActive"></button>
-                    <button id='34' class = "notActive"></button>
-                    <button id='35' class = "notActive"></button>
-                    <button id='36' class = "notActive"></button>
-                    <button id='37' class = "notActive"></button>
-                    <button id='38' class = "notActive"></button>
-                    <button id='39' class = "notActive"></button>
-                    <button id='40' class = "notActive"></button>
-                    <button id='41' class = "notActive"></button>
-                    <button id='42' class = "notActive"></button>
-                    
+                    <button id='1'  class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='2'  class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='3'  class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='4'  class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='5'  class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='6'  class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='7'  class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='8'  class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='9'  class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='10' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='11' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='12' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='13' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='14' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='15' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='16' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='17' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='18' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='19' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='20' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='21' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='22' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='23' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='24' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='25' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='26' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='27' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='28' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='29' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='30' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='31' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='32' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='33' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='34' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='35' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='36' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='37' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='38' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='39' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='40' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='41' class = "notActive" onClick="selecionarDia(this.id)"></button>
+                    <button id='42' class = "notActive" onClick="selecionarDia(this.id)"></button>
                     
                 </div>
             </div>
-            <form action="" method="POST">
-                <input  id="day" type="text" name="dia" value= ''>
-                <input  id="month" type="text" name="mes" value= ''>
-                <input  id="year" type="text" name="ano" value= ''>
-                <input type="submit" name="submit" value="PESQUISAR">
-            </form>
+            
+            <div class="submitSection">
+                <p id="DateText" class = ></p>
+                <form action="" method="POST">
+                    <div class="dateInputs">
+                        <input  id="day" type="text" name="dia" value= ''>
+                        <p>/</p>
+                        <input  id="month" type="text" name="mes" value= ''>
+                        <p>/</p>
+                        <input  id="year" type="text" name="ano" value= ''>    
+                    </div>
+                    <input class="submitInput"type="submit" name="submit" value="Pesquisar" >
+                </form>
+            </div>
 
-            <button onClick="alterarValor()">muda ano</button>
+        
         </div>
     </section>
 
@@ -138,6 +145,7 @@
 
         if($_SERVER['REQUEST_METHOD'] == "POST"){
 
+            $erro = 0;
             //Se o usuário não escolher nenhuma data, o sistema retorna as leituras realizadas no mês atual automaticamente.
 
             if(($_POST['dia'] == "") && ($_POST['mes'] == "") && ($_POST['ano'] == "")){
@@ -149,7 +157,12 @@
             //Se o usuário definir um mês e um ano, sem definir um dia, o sistema mostra as leituras feitas durante todo o mês selecionado.
 
             }else if(($_POST['dia'] == "") && ($_POST['mes'] != "") && ($_POST['ano'] != "")){
-                
+
+                if(intval($_POST['mes']) < 1 || intval($_POST['mes']) > 12){
+                    $erro = 1;?>
+                    <div class="alert error">Mês Inválido</div>
+                <?php    
+                }
                 $data1 = $_POST['ano'] . '-' . $_POST['mes'] . '-1';
                 
                 $data2 = $_POST['ano'] . '-' . $_POST['mes'] . '-31';
@@ -160,14 +173,50 @@
             
             }else if(($_POST['dia'] != "") && ($_POST['mes'] != "") && ($_POST['ano'] != "")){
 
+                if(intval($_POST['mes']) < 1 || intval($_POST['mes']) > 12 ){
+                    $erro = 1;?>
+                    <div class="alert error">Mês Inválido</div>
+                <?php    
+                }else if((intval($_POST['mes']) == 2) && (($_POST['ano'] % 4 == 0) && (($_POST['ano'] % 100 != 0) || ($_POST['ano'] % 400 == 0)))){
+                    if(intval($_POST['dia']) < 1  || intval($_POST['dia']) > 29){
+                        $erro = 1;?>
+                        <div class="alert error">Data Inválida</div>
+                    <?php
+                    }
+                }else if(intval($_POST['mes']) == 2){
+                    if(intval($_POST['dia']) < 1  || intval($_POST['dia']) > 28){
+                        $erro = 1;?>
+                        <div class="alert error">Data Inválida</div>
+                    <?php
+                    }
+                }else if((intval($_POST['mes']) == 1) || (intval($_POST['mes']) == 3) ||(intval($_POST['mes']) == 5) || (intval($_POST['mes']) == 7) || (intval($_POST['mes']) == 8) || (intval($_POST['mes']) == 10) || (intval($_POST['mes']) == 12)){
+                    if(intval($_POST['dia']) < 1  || intval($_POST['dia']) > 31){
+                        $erro = 1;?>
+                        <div class="alert error">Data Inválida</div>
+                    <?php
+                    }
+
+                }else{
+                    if(intval($_POST['dia']) < 1  || intval($_POST['dia']) > 31){
+                        $erro = 1;?>
+                        <div class="alert error">Data Inválida</div>
+                    <?php
+                    }
+                    
+                }
+                
+            
+        
                 $data = $_POST['ano'] . '-' . $_POST['mes'] . '-' . $_POST['dia'];
             
                 $sql = "SELECT * FROM registrosUmidade WHERE data_leitura = '" . $data . "'";
+            
             }
+        
 
 
-
-            $stmt = $PDO->prepare($sql);
+            if($erro != 1){
+                $stmt = $PDO->prepare($sql);
             $stmt->execute();
 
             echo"<div class=\"table\">
@@ -185,6 +234,9 @@
             echo"<div class=\"tableGrid\">";
             
             $cont = 0;
+
+            
+            
             while($linha = $stmt->fetch(PDO::FETCH_OBJ)){
                     
                     if(($cont % 2) == 0){
@@ -200,11 +252,67 @@
                     
                     $cont = $cont + 1;
             }
-            echo "</div>
+            if($cont == 0){
+                echo "</div>
+               </div>
+               
+               <div class=' alert warning'>Sua pesquisa não retornou nenhum resultado</div>
+               ";
+            }else{
+                echo "</div>
                </div>";
-        }
-        ?>
+            }
+            }
+            
+            
+        
+        }                ?>       
     </section>
+
+    <footer style="background-image: url(assets/footer.png)";>
+        <div class="footerLeft">
+            <img src="assets/SIA_w.svg" alt="SIA">
+            <div>
+                <h1>SIA</h1>
+                <p>SISTEMA DE IRRIGAÇÃO AUTOMATIZADO</p>
+            </div>
+        </div>
+
+        <div class="footerRight">
+            <section class="contactProj">
+                <h2>Contato</h2>
+                <p>emailproj@email.com</p>
+                <div>
+                    <a href="#"><img src="assets/icons/github.svg" alt="github"></a>
+                    <a href="#"><img src="assets/icons/instagram.svg" alt="instagram"></a>
+                    <a href="#"><img src="assets/icons/linkedin.svg" alt="linkedin"></a>
+                </div>
+            </section>
+            <section class="contactDevelopers">
+                <h2>Fale Com os Desenvolvedores</h2>
+                <div class="contactContainer">
+                    <div class="contact">
+                        <h3>Joalison Matheus</h3>
+                        <div>
+                            <a href="#"><img src="assets/icons/github.svg" alt="github"></a>
+                            <a href="#"><img src="assets/icons/instagram.svg" alt="instagram"></a>
+                            <a href="#"><img src="assets/icons/linkedin.svg" alt="linkedin"></a>
+                        </div>
+                        <p>joalison@email.com</p>
+                    </div>
+                    <div class="contact">
+                        <h3>Pedro Gustavo</h3>
+                        <div>
+                            <a href="#"><img src="assets/icons/github.svg" alt="github"></a>
+                            <a href="#"><img src="assets/icons/instagram.svg" alt="instagram"></a>
+                            <a href="#"><img src="assets/icons/linkedin.svg" alt="linkedin"></a>
+                        </div>
+                        <p>pedro@email.com</p>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </footer>
     <script src="script.js"></script>
 </body>
 
--- nodemcu/script.js
+++ nodemcu/script.js
@@ -7,35 +7,51 @@ var dia_semana = data.getDay();
 var pdia = new Date(ano, mes, 1);
 var cont = 0;
 var cont = pdia.getDay() + 1;
+document.getElementById('month').value = mes + 1;
+document.getElementById('year').value = ano;
 var meses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
 
+m = document.getElementById("Month");
 function AvancarMes(){
-
+    fadeIn(m,0.8);
     if( mes == 11){
+        fadeIn(document.getElementById("Year"),0.8);
         mes = 0;
         ano +=1;
+        document.getElementById('day').value ='';
+        document.getElementById('month').value = mes + 1;
+        document.getElementById('year').value = ano;
     }else{
         mes +=1;
+        document.getElementById('day').value ='';
+        document.getElementById('month').value = mes + 1;
+        
     }
 
-    document.getElementById("month").innerHTML = meses[mes];
-    document.getElementById("year").innerHTML = ano;
+    document.getElementById("Month").innerHTML = meses[mes];
+    document.getElementById("Year").innerHTML = ano;
     pdia = new Date(ano, mes, 1);
     cont = pdia.getDay() + 1;
     preencherCalendario();
 }   
 
-function VoltarMes(){
-
+ function VoltarMes(){
+    fadeIn(m,0.8);
     if( mes == 0){
+        fadeIn(document.getElementById("Year"),0.8);
         mes = 11;
         ano -=1;
+        document.getElementById('day').value ='';
+        document.getElementById('month').value = mes + 1;
+        document.getElementById('year').value = ano;
     }else{
         mes -=1;
+        document.getElementById('day').value ='';
+        document.getElementById('month').value = mes + 1;
     }
 
-    document.getElementById("month").innerHTML = meses[mes];
-    document.getElementById("year").innerHTML = ano;
+    document.getElementById("Month").innerHTML = meses[mes];
+    document.getElementById("Year").innerHTML = ano;
     pdia = new Date(ano, mes, 1);
     cont = pdia.getDay() + 1;
     preencherCalendario();
@@ -43,24 +59,30 @@ function VoltarMes(){
 
 function limparCalendario(){
 
+    
     for(i = 1; i <= 42; i++){
-        document.getElementById(i).innerHTML = '';
         document.getElementById(i).classList.add('notActive');
+        document.getElementById(i).innerHTML = '';
     }
     
 }
 
 function preencherCalendario(){
+    
+
         limparCalendario();
+        
         if(mes == 1){
             if ((ano % 4 == 0) && ((ano % 100 != 0) || (ano % 400 == 0))){
                 for(i = 1; i <= 29; i++){
+                    f
                     document.getElementById(cont).innerHTML = i;
                     document.getElementById(cont).classList.remove('notActive');
                     cont += 1;
                 }
             }else{
                 for(i = 1; i <= 28; i++){
+                    
                     document.getElementById(cont).innerHTML = i;
                     document.getElementById(cont).classList.remove('notActive');
                     cont += 1;
@@ -68,19 +90,73 @@ function preencherCalendario(){
             }
         }else if((mes == 0) || (mes == 2) || (mes == 4) || (mes == 6) || (mes == 7) || (mes == 09 ) || (mes == 11)){
             for(a = 1; a <= 31; a++){
+                
                 document.getElementById(cont).innerHTML = a;
                 document.getElementById(cont).classList.remove('notActive');
                 cont += 1;
             }
         }else{
             for(i = 1; i <= 30; i++){
+                
                 document.getElementById(cont).innerHTML = i;
                 document.getElementById(cont).classList.remove('notActive');
                 cont += 1;
             }
         }
-
     cont = 0;
+    document.getElementById('DateText').innerHTML = meses[mes] + ' de ' + document.getElementById('year').value;
+        
+}
+preencherCalendario();
+
+function selecionarDia(clikedDay){
+    document.getElementById('day').value = document.getElementById(clikedDay).textContent;
+    document.getElementById('DateText').innerHTML = document.getElementById('day').value + ' de ' + meses[mes] + ' de ' + document.getElementById('year').value;
+};
+
+function arrumarCalendario(){
+    dia = document.getElementById('day').value;
+    mes = document.getElementById('month').value;
+    ano = document.getElementById('year').value;
+    pdia = new Date(ano, mes, 1);
+    cont = pdia.getDay() + 1;
+    preencherCalendario();
 }
 
-preencherCalendario();
+function fadeIn(element,time){
+    processa(element,time,0,100);
+}
+
+
+
+function fadeOut(element,time){
+    processa(element,time,100,0);
+}
+
+
+
+function processa(element,time,initial,end){
+    if(initial == 0){
+             increment = 2;
+    }else {
+             increment = -2;
+    }
+
+
+
+    opc = initial;
+
+
+
+    intervalo = setInterval(function(){
+             if((opc == end)){
+                      if(end == 0){
+                      }
+                      clearInterval(intervalo);
+             }else {
+                      opc += increment;
+                      element.style.opacity = opc/100;
+                      element.style.filter = "alpha(opacity="+opc+")";
+             }
+    },time * 10);
+        }
